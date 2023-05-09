dc7db3e652a0da749d6bf700eaa8ed1616bf1e8b
Eva F <eva.famechon@outlook.com>
2023-05-09 19:57:04 +0200

    Add error 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom'
+import '../styles/style.css'
+
 function Page404() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
+        <div className="error404-container">
+            <h1 className="error-title">404</h1>
+            <span className="error404-message">
+                Oups! La page que vous demandez n'existe pas.
+            </span>
+            <Link className="error404-home-link" to="/">
+                Retourner sur la page d’accueil
+            </Link>
         </div>
     )
 }
